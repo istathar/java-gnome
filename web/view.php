61bf49a481ba5cb8a493022ae2d3afb9e72bf3d2
Andrew Cowie <andrew@operationaldynamics.com>
2006-12-07 18:23:24 +1100

    Parse first line of file as title
    Trim the first line of a doumentation file off and a) use it as the
    <TITLE> in the HTML header, and b) mark it up specially and print it
    as the the top banner of the page.

--- web/view.php
+++ web/view.php
@@ -1,8 +1,13 @@
 <?php
+	$text = file_get_contents("content.txt");
+
+	$first_line_end = strpos($text, "\n");
+	$title = substr($text, 0, $first_line_end);
+	$text = substr($text, $first_line_end);
 ?>
 <html>
 <head>
-<title>Notes</title>
+<title><?=$title?></title>
 <link rel="stylesheet" href="http://research/web.css" type="text/css">
 <style>
 body {
@@ -12,6 +17,11 @@ h1 {
 	background-color: #CCCCCC;
 	border-bottom: 1px solid black;
 	padding-left: 5px;
+}
+h1.title {
+	color: green;
+	font-family: "Arial", sans-serif;
+	font-size: 40px;
 }
 h2 {
 	padding-left: 5px;
@@ -23,11 +33,12 @@ p {
 </style>
 </head>
 <body>
+<h1 class="title"><?=$title?></h1>
+
 <?
 	include_once "markdown.php";
 	include_once "smartypants.php";
 
-	$text = file_get_contents("content.txt");
 	echo SmartyPants(Markdown($text));
 ?>
 </body>
