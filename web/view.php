a042eb035a9a1fa1a53be1af4219a79e2ad9e938
Andrew Cowie <andrew@operationaldynamics.com>
2006-12-07 21:49:47 +1100

    Use the URI to route the request to the appropriate textfile
    Instead of marking up a file whose name is hardcoded, have the PHP
    script parse the URI into 1) project, 2) branch, and then 3) file path
    so that arbitrary text files in a given source tree can be rendered to
    HTML.
    
    This is still custom for my laptop; needs to be converted to the
    website context (and cues made obvious as to how someone can route the
    URI themselves.

--- web/view.php
+++ web/view.php
@@ -1,9 +1,52 @@
 <?php
-	$text = file_get_contents("content.txt");
+#
+# view.php
+# Render a text document marked up with Markdown syntax as an HTML page
+#
+# Copyright (c) 2004-2006 Operational Dynamics Consulting Pty Ltd 
+# 
+# The code in this file, and the library it is a part of, are made available
+# to you by the authors under the terms of the "GNU General Public Licence,
+# version 2". See the LICENCE file for the terms governing usage and
+# redistribution.
+#
+
+#
+# parse the inbound URI. Limit to 4 pieces: 1st is throw away, 2nd is project,
+# 3rd is branch, and the 4th is the remainder of the URI string. Since URI
+# starts with / (as in "/2003/09") [0] in the array is blank!
+#
+
+	$MAX_URI_TOKENS = 4;
+
+	$pieces = explode("/",$_SERVER["REQUEST_URI"], $MAX_URI_TOKENS);
+
+	switch ($pieces[1]) {
+	case "java-gnome":
+		$basedir = "/home/andrew/src/andrew/java-gnome/$pieces[2]";
+		if (!file_exists($basedir)) {
+			echo "<P>Error, can't find \"$basedir\"</P>";
+			exit;
+		}
+		$filepath = "$basedir/$pieces[3]";	
+		break;
+
+	default:
+		echo "<P>Error, don't know how to route \"$pieces[1]\"</P>";
+		exit;
+	}
+
+#
+# now, with the filename in hand, shlurp its contents and parse out the first
+# line as title.
+#
+
+	$text = file_get_contents($filepath);
 
 	$first_line_end = strpos($text, "\n");
 	$title = substr($text, 0, $first_line_end);
 	$text = substr($text, $first_line_end);
+
 ?>
 <html>
 <head>
