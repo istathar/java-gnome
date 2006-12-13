2a266d861beb0ec9e963a0dc757833ba03e3f405
Srichand Pendyala <srichand.pendyala@gmail.com>
2006-12-13 21:45:14 +0530

    Added the setDefaultSize() method to Window.

--- src/java/org/gnome/gtk/Window.java
+++ src/java/org/gnome/gtk/Window.java
@@ -29,9 +29,10 @@ public class Window extends Bin
 
     /**
      * Create a new Window of the specified type. In general you don't need to
-     * use this; see the comments in {@link org.gnome.gtk.WindowType WindowType};
-     * in particular, {@link org.gnome.gtk.WindowType#POPUP POPUP} is <b>not</b>
-     * for dialog windows!
+     * use this; see the comments in
+     * {@link org.gnome.gtk.WindowType WindowType}; in particular,
+     * {@link org.gnome.gtk.WindowType#POPUP POPUP} is <b>not</b> for dialog
+     * windows!
      */
     public Window(WindowType type) {
         super(GtkWindow.createWindow(type));
@@ -43,7 +44,8 @@ public class Window extends Bin
      * The title of a Window is an important usability factor. It should help
      * the user distinguish this Window from others they may have open - and
      * that gets tough when many, many applications are running. The key is to
-     * get the most relevant information is first. Examples of good titles are:
+     * get the most relevant information is first. Examples of good titles
+     * are:
      * <ul>
      * <li><b>Invoice.odt</b>
      * <li><b>Invoice.odt - OpenOffice</b>
@@ -52,11 +54,11 @@ public class Window extends Bin
      * </ul>
      * 
      * <p>
-     * This is important because the list of Windows titles the user is looking
-     * at may have been truncated with the result that you can't tell the
-     * difference between different Windows of the same application. For
-     * example, these are no good if you can only see the first 20 characters of
-     * the title:
+     * This is important because the list of Windows titles the user is
+     * looking at may have been truncated with the result that you can't tell
+     * the difference between different Windows of the same application. For
+     * example, these are no good if you can only see the first 20 characters
+     * of the title:
      * 
      * <ul>
      * <li><b>OpenOffice 2.0.4 brought to yo</b>u by the letter B! -
@@ -105,13 +107,28 @@ public class Window extends Bin
     }
 
     /**
-     * This signal arises when a user tries to close a top level window. As you
-     * would expect, the default handler for this signal destroys the Window.
+     * Sets the default size of a window. If the window's "natural" size (its
+     * size request) is larger than the default, the default will be ignored.
+     * The default size of a window only affects the first time a window is
+     * shown; if a window is hidden and re-shown, it will remember the size it
+     * had prior to hiding, rather than using the default size.
+     * 
+     * resize() could be a more appropriate function. resize() will change the
+     * current size of the window.
+     */
+    public void setDefaultSize(int width, int height) {
+        GtkWindow.setDefaultSize(this, width, height);
+    }
+
+    /**
+     * This signal arises when a user tries to close a top level window. As
+     * you would expect, the default handler for this signal destroys the
+     * Window.
      * 
      * <p>
      * If you want to prevent a Window from being closed, connect this signal,
-     * and return <code>true</code>. Often the reason to do this is to pop up
-     * a notification Dialog, for example asking you if you want to save an
+     * and return <code>true</code>. Often the reason to do this is to pop
+     * up a notification Dialog, for example asking you if you want to save an
      * unsaved document. Another technique is reusing a Window: rather than
      * going to all the trouble to create this Window again, you can just
      * temporarily hide it by calling {@link Widget#hide() Widget's hide()}.
@@ -119,12 +136,13 @@ public class Window extends Bin
      * <p>
      * <i> This signal is actually "delete-event" which lives on GtkWidget.
      * That, however, is for implementation reasons in GTK because all the
-     * GdkEvents go to GtkWidget even though this particular signal only has to
-     * do with Windows. So, we expose it here. </i>
+     * GdkEvents go to GtkWidget even though this particular signal only has
+     * to do with Windows. So, we expose it here. </i>
      * 
      * @author Andrew Cowie
      * @author Devdas Bhagat
      */
+
     public interface DELETE extends GtkWidget.DELETE_EVENT
     {
         public boolean onDeleteEvent(Widget source, Object event);
