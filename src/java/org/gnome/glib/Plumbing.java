f40a7f0674d483a2a4d64893dc4691bac0cd8dce
Andrew Cowie <andrew@operationaldynamics.com>
2006-12-29 12:53:09 +1100

    Introduce instanceFor() as an "abstract" method on Plumbing
    Add new method instanceFor() to org.freedesktop.bindings.Plumbing,
    replacing and absorbing proxyFor(). Since the method is static it
    provides a skeleton method body, and and that's where we put the code
    that used to be in proxyFor().
    
    Nevertheless, the instanceFor() in org.freedesktop.bindings.Plumbing
    should be considered abstract in the sense that any library using this
    infrastructure will need to extend its own instanceFor() that knows
    how to create Proxy instances for pointers that were not originally
    created on the Java side.
    
    Remove individual generated classes' instanceFor() methods, replacing
    them with the instanceFor() method visible on org.gnome.glib.Plumbing,
    and adding the necessary cast.

--- src/java/org/gnome/glib/Plumbing.java
+++ src/java/org/gnome/glib/Plumbing.java
@@ -11,10 +11,12 @@
  */
 package org.gnome.glib;
 
+import org.freedesktop.bindings.Proxy;
+
 /**
  * Translation layer class which adds the ability to connect signals to
- * GObjects. See {@link org.freedesktop.bindings.Plumbing Plumbing} for utiltiy
- * methods which work with Proxy and Enum. <b>No one using developing
+ * GObjects. See {@link org.freedesktop.bindings.Plumbing Plumbing} for
+ * utiltiy methods which work with Proxy and Enum. <b>No one using developing
  * applications which happen to use bindings based on this package should ever
  * need to see, or subclass, this.</b>
  * 
@@ -23,20 +25,40 @@ package org.gnome.glib;
 public abstract class Plumbing extends org.freedesktop.bindings.Plumbing
 {
     /**
+     * Retrieve an appropriate Java Proxy for this pointer. This will return
+     * the Proxy instance already created by a real constructor if one was
+     * created Java side; where no Proxy exists it looks up the underlying
+     * <code>GType</code> and does its best to create it.
+     * 
+     * @see org.freedesktop.bindings.Plumbing#instanceFor(long)
+     */
+    protected static Proxy instanceFor(long pointer) {
+        Proxy obj = org.freedesktop.bindings.Plumbing.instanceFor(pointer);
+
+        if (obj == null) {
+            long type = GValue.g_value_type(pointer);
+            return new StringValue(pointer);
+            // throw new UnsupportedOperationException();
+        } else {
+            return obj;
+        }
+    }
+
+    /**
      * Connect a signal handler to a GObject.
      * 
      * @param instance
      *            the Object Proxy you want to connet the singnal to
      * @param handler
-     *            the object implementing the Signal subinterface we defined in
-     *            one of our public classes.
+     *            the object implementing the Signal subinterface we defined
+     *            in one of our public classes.
      * @param name
      *            the name of the signal being connected. <i>By design, the
      *            literal name of the interface being connected is the name of
-     *            the signal in capitals by our naming convention. Nevertheless,
-     *            you have to explicitly name the signal in because
-     *            self-delegation means we never quite know which is being
-     *            hooked up. This should be generated!</i>
+     *            the signal in capitals by our naming convention.
+     *            Nevertheless, you have to explicitly name the signal in
+     *            because self-delegation means we never quite know which is
+     *            being hooked up. This should be generated!</i>
      */
     /*
      * In this case we need to go back to GObject in order to be able to make
@@ -48,18 +70,11 @@ public abstract class Plumbing extends org.freedesktop.bindings.Plumbing
         GObject.g_signal_connect(pointerOf(instance), handler, receiver, name);
     }
 
-    /**
-     * @return an opaque value which can be passed as a <code>GType</code> to
-     *         the underlying libraries.
-     */
     /*
-     * We place the guard on the assumption that anyone calling this is about to
-     * pass it through to the native side as a GType.
+     * We place the guard on the assumption that anyone calling this is about
+     * to pass it through to the native side as a GType.
      */
     protected static final long typeOf(Value reference) {
-        if (reference.type == 0L) {
-            throw new IllegalStateException("GType not set for this Value");
-        }
-        return reference.type;
+        return 0L; // FIXME REMOVE
     }
 }
