d28c3d8dc1328e8e62361c9c18fb5cadf9ecaecf
Andrew Cowie <andrew@operationaldynamics.com>
2007-01-10 11:49:26 +0800

    Misc Formatting
    Eclipse hackers just fixed a bug in the Code Formatter which caused
    text in <PRE> tags to precess one column to the right each time it was
    invoked. Seems to be fixed.

--- src/java/org/gnome/gtk/Button.java
+++ src/java/org/gnome/gtk/Button.java
@@ -142,8 +142,7 @@ public class Button extends Bin
      * 
      * <p>
      * You can of course create a subclass of Button.CLICKED and then use
-     * instances of it if you have highly complicated algorithms to
-     * implement.
+     * instances of it if you have highly complicated algorithms to implement.
      * 
      * <p>
      * If you implement Button.CLICKED in the class you're currently working
