aa351c7efa5f94f878c7c2dbc4c73d69b50285ca
Andrew Cowie <andrew@operationaldynamics.com>
2007-01-30 22:39:00 +1100

    Remove incorrect code

--- mockup/java/org/gnome/gnome/GnomeEntry.java
+++ mockup/java/org/gnome/gnome/GnomeEntry.java
@@ -1,7 +1,7 @@
 /*
  * GnomeEntry.java
  *
- * Copyright (c) 2006 Operational Dynamics Consulting Pty Ltd
+ * Copyright (c) 2006-2007 Operational Dynamics Consulting Pty Ltd
  * 
  * The code in this file, and the library it is a part of, are made available
  * to you by the authors under the terms of the "GNU General Public Licence,
@@ -21,10 +21,6 @@ import org.gnome.glib.Plumbing;
 
 final class GnomeEntry extends Plumbing
 {
-    static {
-        registerType("GnomeEntry", "org.gnome.gnome.Entry");
-    }
-
     private GnomeEntry() {}
 
     static final String getText(Entry self) {
