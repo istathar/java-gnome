c4c0d8eba72f6bc6ea0fc49c0b4a88f26c8af7b3
Vreixo Formoso <metalpain2002@yahoo.es>
2007-02-10 00:19:11 +0100

    Add improves and some new tests to validate memory management.
    A pair of tests to check that no denaturation occurs and that the object
    retrieved from Gtk+ wrapped methods (such as getChild()...) is the same
    that original object have been implemented.
    
    Also it has been taken into account the new way of removing Java 
    references to GObject by delaying it a bit with a g_iddle_add.
    
    At current time, all test are passed except for the one that checks that 
    cyclic references are managed in the right way. This issue will need 
    some changes in the way signal handlers are managed at this time.
    
    The memory management test still needs some improves to test also what 
    things are being done on Gtk+ side...

--- tests/java/org/gnome/glib/ValidateMemoryManagement.java
+++ tests/java/org/gnome/glib/ValidateMemoryManagement.java
@@ -10,18 +10,21 @@
  */
 package org.gnome.glib;
 
+import java.util.HashSet;
+
 import org.gnome.gtk.Button;
 import org.gnome.gtk.TestCaseGtk;
 import org.gnome.gtk.VBox;
+import org.gnome.gtk.Widget;
+import org.gnome.gtk.Window;
 
 /**
  * Validates memory management by checking many possibly conflicting
  * situations. Note that some situations may exist that are not verified
  * because they cannot be automatically checked from the Java side.
- * 
- * TODO Several tests contemplated here are waiting for a
- * Container.getChildren() implementation.
- * 
+ *  
+ * FIXME we need a way to check what is being occurred on C side!!
+ *  
  * @author Vreixo Formoso
  */
 public class ValidateMemoryManagement extends TestCaseGtk
@@ -37,23 +40,29 @@ public class ValidateMemoryManagement extends TestCaseGtk
          * we need a static field to check object finalization Of course, this
          * not works if many instances are created!!
          */
-        static boolean isFinalized;
+        static HashSet finalized = new HashSet();
 
-        public MyButtonForTesting(String text) {
+        private int code;
+        
+        public MyButtonForTesting(String text, int code) {
             super(text);
-            isFinalized = false;
+            this.code = code;
         }
-
+        
+        public int getCode() {
+            return code;
+        }
+        
         protected void finalize() {
-            System.err.println("finalize() called");
+            System.err.println("finalize() called\t\t" + this.toString());
             System.err.flush();
 
             /*
-             * I prefer having this commented out, because it helps ensure
+             * I prefer having this commented, because it helps ensure
              * that we have a robust design against bad user bad practices
              */
-            // super.finalize();
-            isFinalized = true;
+            super.finalize();
+            finalized.add(new Integer(code));
         }
 
         // ignore
@@ -69,12 +78,14 @@ public class ValidateMemoryManagement extends TestCaseGtk
 
         MyButtonForTesting b;
 
-        b = new MyButtonForTesting("Live free, die young!! Like me");
+        b = new MyButtonForTesting("Live free, die young!! Like me", 1);
 
         cycleMainLoop();
 
         /* object gets out of scope */
         b = null;
+
+        cycleMainLoop();
 
         System.gc();
         try {
@@ -85,7 +96,8 @@ public class ValidateMemoryManagement extends TestCaseGtk
         }
 
         /* check if button has been deleted */
-        assertTrue("An object created but never used is not deleted", MyButtonForTesting.isFinalized);
+        assertTrue("An object created but never used is not deleted", 
+                MyButtonForTesting.finalized.contains( new Integer(1) ));
     }
 
     /**
@@ -98,7 +110,7 @@ public class ValidateMemoryManagement extends TestCaseGtk
         VBox x;
 
         x = new VBox(false, 3);
-        b = new MyButtonForTesting("Old rockers never die!!");
+        b = new MyButtonForTesting("Old rockers never die!!", 2);
 
         /* add button to VBox */
         x.add(b);
@@ -107,6 +119,8 @@ public class ValidateMemoryManagement extends TestCaseGtk
 
         /* object gets out of scope */
         b = null;
+
+        cycleMainLoop();
 
         System.gc();
         try {
@@ -117,7 +131,8 @@ public class ValidateMemoryManagement extends TestCaseGtk
         }
 
         /* check that button hasn't been deleted */
-        assertFalse("Object deleted in Java but still alives in Gtk+", MyButtonForTesting.isFinalized);
+        assertFalse("Object deleted in Java but still alives in Gtk+", 
+                MyButtonForTesting.finalized.contains( new Integer(2) ));
     }
 
     /**
@@ -130,16 +145,22 @@ public class ValidateMemoryManagement extends TestCaseGtk
         VBox x;
 
         x = new VBox(false, 3);
-        b = new MyButtonForTesting("Ready to rock");
+        b = new MyButtonForTesting("Ready to rock", 3);
 
         /* add button to VBox */
         x.add(b);
 
+        cycleMainLoop();
+
         /* out of scope in Gtk+ */
         x.remove(b);
 
+        cycleMainLoop();
+
         /* out of scope in Java */
         b = null;
+
+        cycleMainLoop();
 
         System.gc();
         try {
@@ -150,7 +171,8 @@ public class ValidateMemoryManagement extends TestCaseGtk
         }
 
         /* check that button has been deleted */
-        assertTrue("Object no more needed is not deleted", MyButtonForTesting.isFinalized);
+        assertTrue("Object no more needed is not deleted", 
+                MyButtonForTesting.finalized.contains( new Integer(3) ));
     }
 
     /**
@@ -162,19 +184,27 @@ public class ValidateMemoryManagement extends TestCaseGtk
         VBox x;
 
         x = new VBox(false, 3);
-        b = new MyButtonForTesting("I will reference myself");
+        b = new MyButtonForTesting("I will reference myself", 4);
 
         /* connect button to itself. cyclic ref! */
         b.connect(b);
 
+        cycleMainLoop();
+
         /* add button to VBox */
         x.add(b);
+
+        cycleMainLoop();
 
         /* out of scope in Gtk+ */
         x.remove(b);
 
+        cycleMainLoop();
+
         /* out of scope in Java */
         b = null;
+
+        cycleMainLoop();
 
         System.gc();
         try {
@@ -186,16 +216,113 @@ public class ValidateMemoryManagement extends TestCaseGtk
 
         /* check that button has been deleted */
         assertTrue("Implementation don't take care about cyclic refences",
-                MyButtonForTesting.isFinalized);
+                MyButtonForTesting.finalized.contains( new Integer(4) ));
     }
 
-    // TODO to do this we need Container.getChildren implemented :-(
-    // public final void testNoDenaturation() { }
+    /**
+     * Ensures no denaturation occurs, i.e., object getted from Gtk+
+     * if the same class that original object.
+     */
+    /* 
+     * This test is redundant, because we need not only prevent
+     * denaturation, but indeed get de _same_ object, which is tested
+     * below. Just keep this here to known better what is the problem
+     * if any
+     */
+    public final void testNoDenaturation() { 
+
+        MyButtonForTesting b;
+        Window w;
+
+        w = new Window();
+        b = new MyButtonForTesting("To be or not to be...", 5);
+
+        /* add button to Window */
+        w.add(b);
+
+        cycleMainLoop();
+
+        /* get button from window */
+        Widget wd = w.getChild();
+
+        cycleMainLoop();
+
+        assertNotNull("Retrieved a null object", wd);
+        assertTrue("No a MyButtonForTesting instance. Denaturation occurs", 
+                wd instanceof MyButtonForTesting);
+
+        /* and finally we check object retrieved is the _same_ */
+        assertSame("A different object was created", b, wd);
+        /* now we delete our refs */
+        b = null;
+        wd = null;
+
+        cycleMainLoop();
+
+        System.gc();
+        try {
+            /* this is needed because GC runs asynchronously, I think */
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // just ignore
+        }
+        
+        /* get button again and check */
+        wd = w.getChild();
+    }
 
     /**
      * Ensures no new Java object is created when user retrieves from Gtk a
      * previously referenced Widget.
      */
-    // TODO to do this we need Container.getChildren implemented :-(
-    // public final void testNoMultipleCreation() { }
+    public final void testNoMultipleCreation() { 
+
+        MyButtonForTesting b;
+        Window w;
+
+        w = new Window();
+        b = new MyButtonForTesting("I don't wanna be Dolly", 6);
+
+        /* add button to Window */
+        w.add(b);
+
+        cycleMainLoop();
+
+        /* get button from window */
+        Widget wd = w.getChild();
+
+        cycleMainLoop();
+
+        assertNotNull("Retrieved a null object", wd);
+        assertTrue("No a MyButtonForTesting instance", 
+                wd instanceof MyButtonForTesting);
+
+        /* and finally we check object retrieved is the _same_ */
+        assertSame("A different object was created", b, wd);
+
+        /* now we delete our refs */
+        b = null;
+        wd = null;
+
+        cycleMainLoop();
+
+        System.gc();
+        try {
+            /* this is needed because GC runs asynchronously, I think */
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // just ignore
+        }
+        
+        /* get button again and check */
+        wd = w.getChild();
+
+        assertNotNull("Retrieved a null object", wd);
+        assertTrue("No a MyButtonForTesting instance", 
+                wd instanceof MyButtonForTesting);
+
+        assertEquals("A different object was created", 6, 
+                ( (MyButtonForTesting)wd ).getCode() );
+    }
+
 }
