77c1ebf5d4e2be8b8276c0bd1acc4b3cb1c2620e
Andrew Cowie <andrew@operationaldynamics.com>
2007-04-13 14:24:08 +0530

    Pydev nature int java-gnome Eclipse project
    Hack pydev nature into the Eclipse .project file and add the necessary
    .pydevproject file, taking them from a freshly created blank pydev
    project setup. [what a pain]
    
    Add stub for code generator directory into src/

---
+++ src/codegen/codegen.py
@@ -0,0 +1,9 @@
+#! env python
+#
+# codegen.py
+# 
+# Copyright (c) 2006-2007 Operational Dynamics Consulting Pty Ltd and Others
+# Copyright (c) 2007      Srichand Pendyala
+
+print "This is the beginning"
+
