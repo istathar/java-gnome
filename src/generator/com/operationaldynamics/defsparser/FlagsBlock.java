5304856aac0c33dc7c2fc4d3d0b268ccdc612512
Vreixo Formoso <metalpain2002@yahoo.es>
2007-05-03 23:36:37 +0200

    Add support for flags.
    This adds support for (define-flags ... blocks. Flags are like enums, 
    but their values can't be automatically predicted and thus we need to 
    call to a JNI method for getting it. In public API, flags are to we 
    wrapped as a Constant subclasses, just like enums. 
    
    The Thing for a flag is the same as for a Enum, because both are the 
    same thing and are wrapped in the same way.

---
+++ src/generator/com/operationaldynamics/defsparser/FlagsBlock.java
@@ -0,0 +1,58 @@
+/*
+ * FlagsBlock.java
+ *
+ * Copyright (c) 2007 Operational Dynamics Consulting Pty Ltd, and Others
+ * 
+ * The code in this file, and the library it is a part of, are made available
+ * to you by the authors under the terms of the "GNU General Public Licence,
+ * version 2" See the LICENCE file for the terms governing usage and
+ * redistribution.
+ */
+package com.operationaldynamics.defsparser;
+
+import java.util.List;
+
+import com.operationaldynamics.codegen.FlagsGenerator;
+import com.operationaldynamics.codegen.Generator;
+import com.operationaldynamics.driver.DefsFile;
+
+/**
+ * Block object representing the .defs data defining a Flag. A flag is like 
+ * an enum, but their ordinal values can't be obtained in an automatic way.
+ * 
+ * Source .defs data for an emum is of the following form:
+ * 
+ * <pre>
+ * (define-flags WindowState
+ *   (in-module &quot;Gdk&quot;)
+ *   (c-name &quot;GdkWindowState&quot;)
+ *   (gtype-id "GDK_TYPE_WINDOW_STATE")
+ *   (values
+ *     '(&quot;withdrawn&quot; &quot;GDK_WINDOW_STATE_WITHDRAWN&quot;)
+ *     '(&quot;iconified&quot; &quot;GDK_WINDOW_STATE_ICONIFIED&quot;)
+ *   )
+ * )
+ * </pre>
+ * 
+ * Like Enums, all the information needed to define the Java
+ * code that will result for a Flag type is in a single (define...)
+ * stanza; most other TypeBlocks are accompanied by a series of FunctionBlock
+ * subclasses.
+ * 
+ * @author Vreixo Formoso
+ */
+/*
+ * TODO
+ * I'm using the EnumThing as a FlagThign, cause both are really the same
+ * _thing_.
+ */
+class FlagsBlock extends EnumBlock
+{
+    FlagsBlock(String blockName, List characteristics, List values) {
+        super(blockName, characteristics, values);
+    }
+
+    public Generator createGenerator(final DefsFile data) {
+        return new FlagsGenerator(data, values);
+    }
+}
