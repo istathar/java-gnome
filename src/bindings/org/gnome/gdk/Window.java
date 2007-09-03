ba3ea3302a1967e18eb720bdaa8b96b72aaa4bf5
Andrew Cowie <andrew@operationaldynamics.com>
2007-09-04 01:08:46 +1000

    Expose getState() on Window
    Document the Window class in our GDK package, pointing out the
    considerable differences between theory and practise.
    
    Add a method getState() to org.gnome.gdk.Window to allow developers to
    get the WindowState flags object for the Window.

--- src/bindings/org/gnome/gdk/Window.java
+++ src/bindings/org/gnome/gdk/Window.java
@@ -11,17 +11,55 @@
  */
 package org.gnome.gdk;
 
-/*
- * FIXME this is a placeholder stub for what will become the public API for
- * this type. Replace this comment with appropriate javadoc including author
- * and since tags. Note that the class may need to be made abstract, implement
- * interfaces, or even have its parent changed. No API stability guarantees
- * are made about this class until it has been reviewed by a hacker and this
- * comment has been replaced.
+/**
+ * The underlying native resource driving a rectangular region on a screen.
+ * These are notable as being what powers the display of Widgets, being both
+ * that upon which drawing is done, and that to which user actions are
+ * delivered.
+ * 
+ * <p>
+ * In theory, some, but not all, Widgets have their own native windows to draw
+ * upon and from whence events originate. Thus Widgets such as Buttons have
+ * their own underlying native resource, whereas Labels and various Containers
+ * do not. In practise, this distinction is blurred and newer versions of GTK
+ * do all sorts of strange things under the hood for greater efficiency. You
+ * don't need to worry about any of this.
+ * 
+ * <p>
+ * These are wrappers around Xlib's <code>Window</code> object. They can be
+ * arranged in tree structures, wrapping and overlapping one another, with
+ * parents cropping children, etc. You don't need to worry about any of this
+ * either.
+ * 
+ * <p>
+ * What this <i>is</i> useful for is as a way to get to the state of top
+ * level windows and various low level drawing functions.
+ * 
+ * <p>
+ * <i>Since the C name of this class is <code>GdkWindow</code>, the
+ * unavoidable consequence of the java-gnome mapping algorithm is that the
+ * name of this class Java is Window. This can be a bit of a pain if you're
+ * working in a piece of code where</i> <code>org.gnome.gtk.Window</code>
+ * <i>is already imported, but c'est la vie.</i>
+ * 
+ * @author Andrew Cowie
+ * @since 4.0.4
  */
 public class Window extends Drawable
 {
     protected Window(long pointer) {
         super(pointer);
+    }
+
+    /**
+     * Return a flags object representing the current state of the Window
+     * (maximized, iconified, etc). See {@link WindowState WindowState} for
+     * the individual constants, obviously, but be aware that an unmapped
+     * Window will have no WindowState bits set.
+     * 
+     * @since 4.0.4
+     */
+    public WindowState getState() {
+        return GdkWindow.getState(this);
     }
 }
