e13c2db6390c01ce4d045c5c776f53ad683e5fbc
Andrew Cowie <andrew@operationaldynamics.com>
2007-09-26 20:04:12 +1000

    Bump version to 4.0.5 for next development cycle

--- src/bindings/org/gnome/gtk/Version.java
+++ src/bindings/org/gnome/gtk/Version.java
@@ -30,7 +30,7 @@ final class Version
 {
     static final String APIVERSION = "4.0";
 
-    static final String VERSION = "4.0.4";
+    static final String VERSION = "4.0.5";
 
     private Version() {}
 }
