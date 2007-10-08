f6ec800185d735bd61b7b3bab11ef90683156d6a
Andrew Cowie <andrew@operationaldynamics.com>
2007-10-08 11:01:11 -0400

    Introduce an example to exercise drawing with Cairo
    Needed something to figure out if our binding was actually working, so
    start an example program. This isn't really end-user consumable yet.

---
+++ doc/examples/cairo/ExampleCairoDrawing.java
@@ -0,0 +1,41 @@
+/*
+ * ExampleCairoDrawing.java
+ *
+ * Copyright (c) 2007 Operational Dynamics Consulting Pty Ltd
+ * 
+ * The code in this file, and the library it is a part of, are made available
+ * to you by the authors under the terms of the "GNU General Public Licence,
+ * version 2" See the LICENCE file for the terms governing usage and
+ * redistribution.
+ */
+package cairo;
+
+import org.freedesktop.cairo.Context;
+import org.freedesktop.cairo.Format;
+import org.freedesktop.cairo.ImageSurface;
+import org.gnome.gtk.Gtk;
+
+/**
+ * Exercise drawing with the Cairo API.
+ * 
+ * @author Andrew Cowie
+ */
+public class ExampleCairoDrawing
+{
+    public static void main(String[] args) {
+        final Context cr;
+        final ImageSurface surface;
+
+        Gtk.init(args);
+
+        surface = new ImageSurface(Format.ARGB32, 100, 100);
+        cr = new Context(surface);
+
+        cr.setSourceRGB(0.0, 0.0, 1.0);
+        cr.moveTo(10, 10);
+        cr.lineTo(20, 5);
+        cr.stroke();
+
+        surface.writeToPNG("out.png");
+    }
+}
