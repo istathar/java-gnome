834339d69d77d1963dbff7c400a6d9b7cf107e2d
Andrew Cowie <andrew@operationaldynamics.com>
2008-01-07 11:46:00 +1100

    Update Arch installation instructions
    Merge patch from Timm Preetz updating installation instructions for
    Arch Linux now that java-gnome is more solidly in their distro.

--- web/public/4.0/get/arch.php
+++ web/public/4.0/get/arch.php
@@ -2,7 +2,7 @@
 /*
  * arch.php
  *
- * Copyright (c) 2007 Operational Dynamics Consulting Pty Ltd
+ * Copyright (c) 2007-2008 Operational Dynamics Consulting Pty Ltd, and Others
  * 
  * This file comprises part of the infrastructure and content of the
  * java-gnome project website. As such, it is conveyed alongside the source
@@ -27,59 +27,22 @@
 ?>
 <h1 class="title">Installing java-gnome on Arch Linux</h1>
 
-<p>Package files now exist to allow you to easily install the java-gnome
-library on an Arch Linux system.</p>
+<p>Packages now exist to allow you to easily install the java-gnome library
+on an Arch Linux system. The packages are in [community], so you need
+to have the [community]-repository enabled.</p>
 
-<h1>Installing java-gnome from AUR</h1>
+<p>The following command should install the latest released version of the
+bindings:</p>
 
-<p>As java-gnome is not yet in [extra] or [community] you will need to fetch
-the PKGBUILD and build the package yourself. But don't worry, this is easily
-done in four steps.</p>
-
-<p>First, create a build directory for java-gnome and change into it</p>
 <pre>
-$ mkdir java-gnome_builddir && cd java-gnome_builddir
+# pacman -S java-gnome
 </pre>
 
-<p>Now fetch the PKGBUILD</p>
+<p>To also install the docs, for use in <code>devhelp</code> for example, just
+run:</p>
+
 <pre>
-$ wget http://aur.archlinux.org/packages/java-gnome/java-gnome/PKGBUILD
-</pre>
-
-<p>Build the package with <i>makepkg</i></p>
-<pre>
-$ makepkg
-</pre>
-
-<p>Finally, login as root (using <i>su</i>) and install the package</p>
-<pre>
-# pacman -U java-gnome-*.pkg.tar.gz
-</pre>
-
-<h1>Installing java-gnome-docs from AUR</h1>
-
-<p>If you would like to read the java-gnome documentation offline, you can also
-get the java-gnome-docs package from AUR.</p>
-
-<p>Again, we create a builddir for java-gnome-docs and switch into it</p>
-<pre>
-$ mkdir java-gnome-docs_builddir && cd java-gnome-docs_builddir
-</pre>
-
-<p>Now we will fetch the build-instructions package which includes the
-PKGBUILD</p>
-<pre>
-$ wget http://aur.archlinux.org/packages/java-gnome-docs/java-gnome-docs.tar.gz
-</pre>
-
-<p>Extract that file and build the package with <i>makepkg</i></p>
-<pre>
-$ tar -xfvz java-gnome-docs.tar.gz && makepkg
-</pre>
-
-<p>Finally, login as root (using <i>su</i>) and install the package</p>
-<pre>
-# pacman -U java-gnome-docs*.pkg.tar.gz
+# pacman -S java-gnome-docs
 </pre>
 
 <?
