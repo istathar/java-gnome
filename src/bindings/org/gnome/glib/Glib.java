83253ab11f4f6a1b0671ac3a4d97a1e992b28a43
Andrew Cowie <andrew@operationaldynamics.com>
2008-01-14 14:06:38 +1100

    Expose setProgramName() as a utility function on Glib
    We call g_set_prgname() automatically in our Gtk.main()
    implementation, but always blaming "java-gnome" isn't the best optics.
    So expose setProgramName() as a function in the Glib class. Maybe not
    the best place for it, but it's a start. Add a GlibMisc.defs
    containing the declaration information.

--- src/bindings/org/gnome/glib/Glib.java
+++ src/bindings/org/gnome/glib/Glib.java
@@ -78,7 +78,7 @@ public class Glib
      */
     /*
      * TODO make it possible for non GTK libraries to initialize java-gnome.
-     * This will involve moving the System.loadlLibrary() call here, and more
+     * This will involve moving the System.loadLibrary() call here, and more
      * importantly adding a JNI call here to do the GThreads and related
      * setup. For now, keeping the requirement as Gtk.init() [and hence the
      * JNI code in src/bindings/org/gnome/gtk/Gtk.c] is fine; running it
@@ -88,5 +88,40 @@ public class Glib
         if (!initialized) {
             throw new Error("\n\nYou *must* call Gtk.init() before using anything else in java-gnome!\n");
         }
+    }
+
+    /**
+     * Change the internal program name used by GLib and GTK for internal
+     * error messages. Occasionally (especially as we develop new
+     * functionality) you or we will do something wrong, and GLib will
+     * complain loudly about it to the console, for example:
+     * 
+     * <pre>
+     * (gnome-panel:5581): Gtk-WARNING **: gtk_widget_size_allocate(): attempt to...
+     * </pre>
+     * 
+     * where "<code>gnome-panel</code>" was the name set by that program
+     * with this method call, and <code>5581</code> was the process id
+     * originating the message. As you can see, the whole thing is pretty ugly
+     * (not to mention having no context), which is why one of the design
+     * goals of java-gnome is to fully proxy the entire underlying library and
+     * have none of the internals from GLib or GTK be exposed to the Java
+     * developer. If we do our job right, your users should never see a
+     * message like that; at <i>worst</i> it would be reported as a Java
+     * stack trace.
+     * 
+     * <p>
+     * You don't really need to call this, but it's here if you want to make
+     * it clearer in the <code>.xsession-errors</code> log what the culprit
+     * application is. The default name is "java", which is fine until you
+     * deploy for production use.
+     * 
+     * @since 4.0.6
+     */
+    /*
+     * Another one to potentially move to a GtkApplication class.
+     */
+    public static void setProgramName(String name) {
+        GlibMisc.setPrgname(name);
     }
 }
