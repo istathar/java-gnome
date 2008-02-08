69b12826c67054ea1b9bdf44251bfd2f68e85e28
Andrew Cowie <andrew@operationaldynamics.com>
2008-02-08 17:45:30 +1100

    Expose and document flush() and finish() on Surface
    After conversation with Carl Worth in #gtk+, document the usage of the
    flush() and finish() methods available on Surface, adding the .defs
    data for flush and implementing the binding method.

--- src/bindings/org/freedesktop/cairo/Surface.java
+++ src/bindings/org/freedesktop/cairo/Surface.java
@@ -1,7 +1,7 @@
 /*
  * Surface.java
  *
- * Copyright (c) 2007 Operational Dynamics Consulting Pty Ltd
+ * Copyright (c) 2007-2008 Operational Dynamics Consulting Pty Ltd
  *
  * The code in this file, and the library it is a part of, are made available
  * to you by the authors under the terms of the "GNU General Public Licence,
@@ -29,11 +29,57 @@ public abstract class Surface extends Proxy
     protected void release() {
         CairoSurface.destroy(this);
     }
-    
+
+    /**
+     * Indicate to Cairo that you are finished drawing on this Surface and
+     * that it can release the resources that Cairo has used in association
+     * with it. After you call this, the Surface and any Contexts associated
+     * with it will be in-operative.
+     * 
+     * <p>
+     * While the virtual machine will of course finalize this object when you
+     * no longer have strong Java references to it, garbage collection is
+     * non-deterministic and often takes place long after drawing is
+     * completed. Calling this can allow the underlying library to get on with
+     * releasing the resources involved in what is, after all, meant to be a
+     * very fast and transient operation.
+     * 
+     * @since 4.0.7
+     */
     public void finish() {
         CairoSurface.finish(this);
     }
-    
+
+    /**
+     * Complete any pending drawing operations. If your Surface comes from a
+     * subsystem whose resources are shared with other drawing libraries, then
+     * you need to call this before letting control return to a point where
+     * those libraries might draw on the same backing Surface.
+     * 
+     * <p>
+     * Quoting Carl Worth, the original author of Cairo, <blockquote>This is
+     * really only necessary to call if you want to switch from Cairo-based
+     * rendering to non-Cairo-based rendering to the same underlying thingy
+     * under the Surface (Window, Pixmap, data buffer, etc.).</blockquote>
+     * 
+     * <p>
+     * In other words, if you are the only one drawing on the Surface, then
+     * you don't need this. And that is indeed the case if you are drawing on
+     * your own custom Widget in its <code>EXPOSE_EVENT</code> handler.
+     * 
+     * <p>
+     * See also {@link #finish() finish()} if you're just trying to day "I'm
+     * done".
+     * 
+     * <p>
+     * <i>Clearly, "thingy" is an advanced graphics term.</i>
+     * 
+     * @since 4.0.7
+     */
+    public void flush() {
+        CairoSurface.flush(this);
+    }
+
     public void writeToPNG(String filename) {
         CairoSurface.writeToPng(this, filename);
     }
