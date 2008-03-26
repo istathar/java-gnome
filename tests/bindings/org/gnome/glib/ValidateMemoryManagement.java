fb18112bbbdf7e71f7746a99412a23d2296d60a1
Vreixo Formoso <metalpain2002@yahoo.es>
2008-03-26 21:39:17 +0100

    Add a test case that show a memory management problem.
    The problem is related with GObjects created by Gtk+. When they are 
    returned for first time, we create a Proxy for them, adding the toggle 
    ref and removing the original ref. The problem is that in some functions 
    we actually don't own that ref, so we are droping a Gtk+ ref. When the 
    proxy goes out of scope, it has no ref from Gtk+ side (only the toggle 
    ref), and it is freed. Next time we try to get the same object, an 
    incorrect pointer is returned, thus leading to unexpected behavior, 
    probably SEGFAULT.

--- tests/bindings/org/gnome/glib/ValidateMemoryManagement.java
+++ tests/bindings/org/gnome/glib/ValidateMemoryManagement.java
@@ -15,6 +15,8 @@ import java.util.HashSet;
 
 import org.gnome.gtk.Button;
 import org.gnome.gtk.TestCaseGtk;
+import org.gnome.gtk.TreeSelection;
+import org.gnome.gtk.TreeView;
 import org.gnome.gtk.VBox;
 import org.gnome.gtk.ValidatePacking;
 import org.gnome.gtk.ValidateProperties;
@@ -368,5 +370,32 @@ public class ValidateMemoryManagement extends TestCaseGtk
         assertTrue("Not a MyButton instance", c instanceof MyButton);
 
         assertEquals("A different object was created", 6, ((MyButton) c).getCode());
+    }
+
+    /**
+     * Check that objects that are created internally in Gtk+ are correctly
+     * managed.
+     */
+    public final void testCorrectHandlingOfGtkCreatedObjects() {
+        TreeView t;
+        TreeSelection s;
+
+        t = new TreeView();
+        cycleMainLoop();
+        
+        /* get the selection, a Gtk+ created object */
+        s = t.getSelection();
+        cycleMainLoop();
+        
+        /* free our ref */
+        s = null;
+        cycleMainLoop();
+        cycleGarbageCollector();
+        
+        /* 
+         * if all was correct, we can retrieve it again. 
+         */
+        s = t.getSelection();
+        cycleMainLoop();
     }
 }
