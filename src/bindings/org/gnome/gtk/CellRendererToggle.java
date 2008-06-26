804ec3309487aafe11c33aa51a609d91de4a8e67
Andrew Cowie <andrew@operationaldynamics.com>
2008-06-26 12:28:09 +1000

    Formatting and since tags

--- src/bindings/org/gnome/gtk/CellRendererToggle.java
+++ src/bindings/org/gnome/gtk/CellRendererToggle.java
@@ -12,78 +12,88 @@
 package org.gnome.gtk;
 
 /**
- * Renders a Toggle Button into a TreeViewColumn. This CellRenderer may be used 
- * to present boolean data to the user. The fundamental mapping method is 
- * {@link #setActive(DataColumnBoolean) setActive()} which you use to indicate the
- * particular DataColumnBoolean from the underlying TreeModel which will provide
- * the boolean values.
+ * Renders a Toggle Button into a TreeViewColumn. This CellRenderer may be
+ * used to present boolean data to the user. The fundamental mapping method is
+ * {@link #setActive(DataColumnBoolean) setActive()} which you use to indicate
+ * the particular DataColumnBoolean from the underlying TreeModel which will
+ * provide the boolean values.
  * 
  * @author Andreas Kuehntopf
+ * @since 4.0.8
  */
 public class CellRendererToggle extends CellRenderer
 {
     /**
-     * Construct a new CellRendererToggle. Specify the TreeViewColumn
-     * it will belong to.
+     * Construct a new CellRendererToggle. Specify the TreeViewColumn it will
+     * belong to.
+     * 
+     * @since 4.0.8
      */
     public CellRendererToggle(TreeViewColumn vertical) {
         // FIXME does expand to false actually make sense?
         super(GtkCellRendererToggle.createCellRendererToggle(), vertical, false);
     }
-    
+
     /**
-     * Indicate the appearance of this CellRenderer. If
-     * this is set to true a radio button is used instead of
-     * the default check box.
+     * Indicate the appearance of this CellRenderer. If this is set to true a
+     * radio button is used instead of the default check box.
+     * 
+     * @since 4.0.8
      */
     public void setRadio(boolean radio) {
-        GtkCellRendererToggle.setRadio(this, true);                
+        GtkCellRendererToggle.setRadio(this, true);
     }
 
     /**
-     * Indicate if the CellRenderer's toggle button
-     * should be active.
+     * Indicate if the CellRenderer's toggle button should be active.
      * 
      * <p>
-     * If you want to bind the state of the toggle button
-     * to a DataColumn you might want to use the overloaded method
+     * If you want to bind the state of the toggle button to a DataColumn you
+     * might want to use the overloaded method
      * {@link #setActive(DataColumnBoolean) setActive()}
+     * 
+     * @since 4.0.8
      */
     public void setActive(boolean active) {
         GtkCellRendererToggle.setActive(this, active);
     }
-    
+
     /**
      * Get the current state of the toggle button
+     * 
+     * @since 4.0.8
      */
     public boolean getActive() {
         return GtkCellRendererToggle.getActive(this);
     }
-    
+
     /**
-     * Indicate the DataColumn the state of the CellRendererBoolean is
-     * bound to. This is the mapping method you should use if you want to bind
-     * the toggle button's state to a DataColumn's value.      
+     * Indicate the DataColumn the state of the CellRendererBoolean is bound
+     * to. This is the mapping method you should use if you want to bind the
+     * toggle button's state to a DataColumn's value.
+     * 
+     * @since 4.0.8
      */
     public void setActive(DataColumnBoolean column) {
-        GtkCellLayout.addAttribute(vertical, this, "active", column.getOrdinal());       
+        GtkCellLayout.addAttribute(vertical, this, "active", column.getOrdinal());
     }
-        
+
     /**
-     * Event generated after user toggles the renderered 
-     * toggle button in a Cell.
+     * Signal generated after user toggles the renderered toggle button in a
+     * Cell.
      * 
      * <p>
-     * Note that the act of toggling the cell does not cause a 
-     * change in the underlying model.
+     * Note that the act of toggling the cell does not cause a change in the
+     * underlying model.
      * 
      * @author Andreas Kuehntopf
+     * @since 4.0.8
      */
     public interface TOGGLED
     {
         public void onToggled(CellRendererToggle source, TreePath path);
     }
-    
+
     /**
      * Hook up a signal handler to receive "toggled" events on this
      * CellRenderer. A typical example of how this is used is as follows:
@@ -109,24 +119,26 @@ public class CellRendererToggle extends CellRenderer
      *                  renderer.getActive());
      *       }});
      * </pre>
+     * 
+     * @since 4.0.8
      */
     public void connect(TOGGLED handler) {
         GtkCellRendererToggle.connect(this, new ToggledHandler(handler));
     }
-    
+
     /*
      * Helper class to convert second parameter from String to TreePath
      */
     private static class ToggledHandler implements GtkCellRendererToggle.TOGGLED
     {
         private TOGGLED handler;
-        
+
         public ToggledHandler(TOGGLED handler) {
             this.handler = handler;
         }
 
         public void onToggled(CellRendererToggle source, String path) {
             handler.onToggled(source, new TreePath(path));
-        }        
-    }    
+        }
+    }
 }
