deaba85936a62863b1a3522159df43924f3a0395
Andrew Cowie <andrew@operationaldynamics.com>
2008-08-26 19:37:40 +1000

    Clean example back to a single constructor
    For readability when online, refactor the example back to all the UI
    setup code being in a single constructor.
    
    Correct spelling of class name.

--- doc/examples/textview/ExampleInstantMessenger.java
+++ doc/examples/textview/ExampleInstantMessenger.java
@@ -28,6 +28,7 @@ import org.gnome.gtk.VBox;
 import org.gnome.gtk.Widget;
 import org.gnome.gtk.Window;
 import org.gnome.pango.Style;
+import org.gnome.pango.Weight;
 
 import static org.freedesktop.bindings.Time.formatTime;
 
@@ -40,49 +41,27 @@ import static org.freedesktop.bindings.Time.formatTime;
  * @author Andrew Cowie
  * @author Stefan Prelle
  */
-public class ExampleInstantMessager
+public class ExampleInstantMessenger
 {
-    private TextView incoming;
+    private final TextView incoming;
 
-    private Entry outgoing;
+    private final TextBuffer buffer;
 
-    private Window window;
+    private final Pixbuf smiley;
 
-    private VBox top;
+    private final TextTag gray, italics, blue;
 
-    private Pixbuf smiley;
+    private ExampleInstantMessenger() throws FileNotFoundException {
+        final Window window;
+        final VBox top;
+        final Thread other;
+        final Entry outgoing;
 
-    private TextTag gray, italics, blue;
+        /*
+         * Start with the usual establishment of a Window to contain the
+         * example.
+         */
 
-    public static void main(String[] args) {
-        Gtk.init(args);
-
-        new ExampleInstantMessager();
-
-        Gtk.main();
-    }
-
-    /*
-     * This class also demonstrates an alternative way to manage building the
-     * user interface: a series of sequential methods. This is mostly
-     * cosmetic, but as you can see it does allow you to follow the logic of
-     * what is going on rather than the constructor being hundreds of lines
-     * long. More to the point, it makes navigation in an IDE fare more
-     * effective.
-     */
-    private ExampleInstantMessager() {
-        loadImages();
-        setupInitial();
-        createFormatTags();
-        setupIncomingDisplay();
-        setupOutgoingEntry();
-        initialPresentation();
-    }
-
-    /**
-     * The usual establishment of a Window to contain the example.
-     */
-    private void setupInitial() {
         window = new Window();
         window.setTitle("Instant Messaging");
         window.setSizeRequest(300, 200);
@@ -96,17 +75,14 @@ public class ExampleInstantMessager
                 return false;
             }
         });
-    }
 
-    /**
-     * Create a TextView which will display incoming text messages (and also
-     * echo messages as they are sent). It is set up to be read only and to
-     * not have a cursor, thereby conveying the impression that it is just a
-     * display (a cursor especially would suggest that the text there can be
-     * changed).
-     */
-    private void setupIncomingDisplay() {
-        TextBuffer buffer;
+        /*
+         * Create a TextView which will display incoming text messages (and
+         * also echo messages as they are sent). It is set up to be read only
+         * and to not have a cursor, thereby conveying the impression that it
+         * is just a display (a cursor especially would suggest that the text
+         * there can be changed).
+         */
 
         buffer = new TextBuffer();
 
@@ -116,34 +92,17 @@ public class ExampleInstantMessager
         incoming.setPaddingBelowParagraph(10);
 
         top.packStart(incoming, true, true, 0);
-    }
 
-    /**
-     * TextTags are how you apply formatting to content. We'll create a few
-     * for later use in the display.
-     */
-    private void createFormatTags() {
-        gray = new TextTag();
-        gray.setForeground("gray");
+        /*
+         * Create the place for the user to enter messages they want to send.
+         * 
+         * The interesting part here is not that there is an Entry (a real
+         * Instant Messager would have a TextView supporting rich content area
+         * for the user to write messages too) but that when the user presses
+         * Enter in the Entry it "sends" a message and appends it to the log
+         * in the incoming TextView.
+         */
 
-        italics = new TextTag();
-        italics.setStyle(Style.ITALIC);
-        italics.setForeground("green");
-
-        blue = new TextTag();
-        blue.setForeground("blue");
-    }
-
-    /**
-     * Create the place for the user to enter messages they want to send.
-     * 
-     * The interesting part here is not that there is an Entry (a real Instant
-     * Messager would have a TextView supporting rich content area for the
-     * user to write messages too) but that when the user presses Enter in the
-     * Entry it "sends" a message and appends it to the log in the incoming
-     * TextView.
-     */
-    private void setupOutgoingEntry() {
         outgoing = new Entry();
         top.packStart(outgoing, false, false, 0);
 
@@ -168,99 +127,29 @@ public class ExampleInstantMessager
                 outgoing.setText("");
             }
         });
-    }
-
-    private void loadImages() {
-        try {
-            smiley = new Pixbuf("doc/examples/textview/face-smile.png");
-        } catch (FileNotFoundException fnfe) {
-            System.err.println("Sorry " + fnfe.getMessage());
-            System.exit(1);
-        }
-    }
-
-    /**
-     * Append a received (or sent) message to the incoming display.
-     */
-    /*
-     * For fun, we translate the smile emoticon into an image, giving us an
-     * opportunity to demonstrate adding non-text elements to a TextBuffer.
-     */
-    private void appendMessage(String msg, boolean outgoing) {
-        final TextBuffer text;
-        final TextIter end;
-        final long now;
-        final String timestamp;
-        final TextTag colour;
-        int i;
-        int prev;
 
         /*
-         * Get a TextIter pointing at the end of the existing text.
+         * TextTags are how you apply formatting to content. We'll create a
+         * few for later use in the display.
          */
 
-        text = incoming.getBuffer();
-        end = text.getIterEnd();
+        gray = new TextTag();
+        gray.setForeground("#777777");
+
+        italics = new TextTag();
+        italics.setStyle(Style.ITALIC);
+        italics.setForeground("darkgreen");
+
+        blue = new TextTag();
+        blue.setWeight(Weight.BOLD);
+        blue.setForeground("blue");
 
         /*
-         * Start with a paragraph separator and a timestamp. We colour the
-         * timestamp a lighter colour so as to not distract from the text.
+         * And finally load an image that we'll use later to replace text
+         * smilies with.
          */
 
-        text.insert(end, "\n");
-
-        now = System.currentTimeMillis() / 1000;
-        timestamp = formatTime("%H:%M:%S\t", now);
-        text.insert(end, timestamp, gray);
-
-        /*
-         * Loop over what we're going to add, replacing text smileys with
-         * graphical ones. As for the text we write, if the user sent it we'll
-         * make it blue but if incoming we'll leave it black.
-         */
-
-        if (outgoing) {
-            colour = blue;
-        } else {
-            colour = null;
-        }
-
-        prev = 0;
-
-        while ((i = msg.indexOf(":)", prev)) != -1) {
-            text.insert(end, msg.substring(prev, i), colour);
-            text.insert(end, smiley);
-
-            i += 2;
-            prev = i;
-        }
-        text.insert(end, msg.substring(prev), colour);
-    }
-
-    /**
-     * When a conversation starts we want to indicate who it is with.
-     */
-    private void startConversationWith(String who) {
-        final TextBuffer text;
-        final TextIter pointer;
-
-        /*
-         * You've probably realized by now that it would be a lot easier to
-         * just keep a field with a reference to the TextBuffer model.
-         */
-        text = incoming.getBuffer();
-
-        /*
-         * This hard codes the assumption that this is only being called once,
-         * before any calls to appendMessage().
-         */
-
-        pointer = text.getIterStart();
-        text.insert(pointer, "Starting conversation with " + who, italics);
-    }
-
-    private void initialPresentation() {
-        final Thread other;
+        smiley = new Pixbuf("doc/examples/textview/face-smile.png");
 
         /*
          * Put the Window and all its children on screen.
@@ -280,21 +169,97 @@ public class ExampleInstantMessager
          * And start the "converstaion" :)
          */
 
-        other = new Conversation();
+        other = new IncomingConversation();
         other.start();
     }
 
-    class Conversation extends Thread
+    /**
+     * Append a received (or sent) message to the incoming display.
+     */
+    /*
+     * For fun, we translate the smile emoticon into an image, giving us an
+     * opportunity to demonstrate adding non-text elements to a TextBuffer.
+     */
+    private void appendMessage(String msg, boolean outgoing) {
+        final TextIter end;
+        final long now;
+        final String timestamp;
+        final TextTag colour;
+        int i;
+        int prev;
+
+        /*
+         * Get a TextIter pointing at the end of the existing text.
+         */
+
+        end = buffer.getIterEnd();
+
+        /*
+         * Start with a paragraph separator and a timestamp. We colour the
+         * timestamp a lighter colour so as to not distract from the text.
+         */
+
+        buffer.insert(end, "\n");
+
+        now = System.currentTimeMillis() / 1000;
+        timestamp = formatTime("%H:%M:%S\t", now);
+        buffer.insert(end, timestamp, gray);
+
+        /*
+         * Loop over what we're going to add, replacing text smileys with
+         * graphical ones. As for the text we write, if the user sent it we'll
+         * make it blue but if incoming we'll leave it black.
+         */
+
+        if (outgoing) {
+            colour = blue;
+        } else {
+            colour = null;
+        }
+
+        prev = 0;
+
+        while ((i = msg.indexOf(":)", prev)) != -1) {
+            buffer.insert(end, msg.substring(prev, i), colour);
+            buffer.insert(end, smiley);
+
+            i += 2;
+            prev = i;
+        }
+        buffer.insert(end, msg.substring(prev), colour);
+    }
+
+    /**
+     * When a conversation starts we want to indicate who it is with.
+     */
+    private void startConversationWith(String who) {
+        final TextIter pointer;
+
+        /*
+         * Place a message at the top of the display indicating who is writing
+         * in.
+         */
+
+        pointer = buffer.getIterStart();
+        buffer.insert(pointer, "Starting conversation with " + who, italics);
+    }
+
+    class IncomingConversation extends Thread
     {
         private final String[] messages;
 
-        Conversation() {
+        private IncomingConversation() {
+
             /*
              * Mark this thread as a daemon thread, else the main thread
              * terminating after Gtk.main() returns will not end the program.
              */
 
             this.setDaemon(true);
+
+            /*
+             * Some silly messages to use to fake incoming conversation.
+             */
 
             messages = new String[] {
                     "Hello there!",
@@ -303,13 +268,13 @@ public class ExampleInstantMessager
                     "Why won't you talk to me?"
             };
 
-            startConversationWith("Jane");
+            startConversationWith("Jane Doe");
         }
 
         public void run() {
             for (int i = 0; i < messages.length; i++) {
                 try {
-                    sleep((int) (1000 + i * 1000 * Math.random()));
+                    sleep((int) (1000 + i * 2000 * Math.random()));
 
                 } catch (InterruptedException ie) {
                 }
@@ -317,5 +282,18 @@ public class ExampleInstantMessager
                 appendMessage(messages[i], false);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Gtk.init(args);
+
+        try {
+            new ExampleInstantMessenger();
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Sorry " + fnfe.getMessage());
+            System.exit(1);
+        }
+
+        Gtk.main();
     }
 }
