ba2959f8979ac795deefdff0542122e86d5d353b
Andrew Cowie <andrew@operationaldynamics.com>
2008-10-26 18:53:55 +1100

    Documentation for Clipboard
    Fix typos and then add prose giving an overview, describing which
    clipboard is returned by getDefault(), adding a link to the
    FreeDesktop spec page, and a basic usage snippet.

--- src/bindings/org/gnome/gtk/Clipboard.java
+++ src/bindings/org/gnome/gtk/Clipboard.java
@@ -14,41 +14,78 @@ package org.gnome.gtk;
 import org.gnome.gdk.Pixbuf;
 import org.gnome.glib.Object;
 
-/*
- * Clipboard object holds data to be shared between different processes.
- * The clipboard object covered in java-gnome is the default clipboard
- * and its data can be later used by being pasted into another application.
- *
+/**
+ * A mechanism allowing a user to copy data between programs.
+ * 
+ * <p>
+ * Clipboards were originally for sharing data between different processes,
+ * but of course conventional practise these days is that cut, copy and paste
+ * are used quite ubiquitously both as a way to copy things from one program
+ * to another, as well as simply moving data around within an application.
+ * 
+ * <p>
+ * You can copy text to the main system Clipboard quite simply, as follows:
+ * 
+ * <pre>
+ * clipboard = Clipboard.getDefault();
+ * clipboard.setText(&quot;Hello there&quot;);
+ * </pre>
+ * 
  * @author Serkan Kaba
+ * @author Andrew Cowie
  * @since 4.0.10
+ * @see <a
+ *      href="http://www.freedesktop.org/wiki/Specifications/clipboards-spec">The
+ *      gory historical details that lead to the FreeDesktop clipboard
+ *      specification</a>
+ */
+/*
+ * TODO The underlying GtkClipboard is wildly complex and very powerful,
+ * allowing for asynchronous data transfer. If anything, it seems a bit
+ * overboard, and perhaps a simpler synchronous implementation would be handy.
+ * Regardless, someone will have to manually set up signals to proxy for these
+ * callbacks as at time of writing we don't have a [generated] way to
+ * automatically represent function pointers. See the native code underneath
+ * TreeModelFilter's setVisibleCallback() for an example.
  */
 public class Clipboard extends Object
 {
     protected Clipboard(long pointer) {
         super(pointer);
     }
-    
+
     /**
-     * Returns the default (GDK_SELECTION_CLIPBOARD) clipboard object.
-     *
+     * Get the default Clipboard.
+     * 
+     * <p>
+     * <i>There are actually quite a number of different clipboards in the X
+     * server! Normal people are used to there only being one, and current
+     * conventions are that the</i> <code>CLIPBOARD</code> <i>clipboard is the
+     * one that cut, copy, and paste operations go to. That is the clipboard
+     * returned here.</i>
+     * 
      * @since 4.0.10
+     */
+    /*
+     * FUTURE, so, obviously, if someone desperately needs access to PRIMARY,
+     * they can expose it as getPrimary().
      */
     public static Clipboard getDefault() {
         return GtkClipboardOverride.get();
     }
-    
+
     /**
-     * Copies given text to clipboard.
-     *
+     * Copy the given text to this Clipboard.
+     * 
      * @since 4.0.10
      */
     public void setText(String text) {
         GtkClipboard.setText(this, text, -1);
     }
-    
+
     /**
-     * Copies given image to clipboard.
-     *
+     * Copy the given image to this Clipboard.
+     * 
      * @since 4.0.10
      */
     public void setImage(Pixbuf pixbuf) {
