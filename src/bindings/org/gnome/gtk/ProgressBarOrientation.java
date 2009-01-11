c452bb536aaf0d78f9eabfe383ca9e2e6a7787af
Miloud Bel. <Goundy@gmail.com>
2009-01-11 20:33:54 +0100

    Added ProgressBarorientation support

--- src/bindings/org/gnome/gtk/ProgressBarOrientation.java
+++ src/bindings/org/gnome/gtk/ProgressBarOrientation.java
@@ -1,7 +1,7 @@
 /*
  * ProgressBarOrientation.java
  *
- * Copyright (c) 2007 Operational Dynamics Consulting Pty Ltd
+ * Copyright (c) 2009 Operational Dynamics Consulting Pty Ltd
  *
  * The code in this file, and the library it is a part of, are made available
  * to you by the authors under the terms of the "GNU General Public Licence,
@@ -14,18 +14,27 @@ package org.gnome.gtk;
 import org.freedesktop.bindings.Constant;
 
 /*
- * FIXME this is a placeholder stub for what will become the public API for
- * this type. Replace this comment with appropriate javadoc including author
- * and since tags. Note that the class may need to be made abstract, implement
- * interfaces, or even have its parent changed. No API stability guarantees
- * are made about this class until it has been reviewed by a hacker and this
- * comment has been replaced.
+ * Request that the progressbar progression be performed in a given direction.
+ * 
+ * @since 4.0.10
  */
 public final class ProgressBarOrientation extends Constant
 {
+    /*
+     * Perform progress from left to right
+     */
     public static final ProgressBarOrientation LEFT_TO_RIGHT = new ProgressBarOrientation(GtkProgressBarOrientation.LEFT_TO_RIGHT, "LEFT_TO_RIGHT");
+    /*
+     * Perform progress from right to left
+     */
     public static final ProgressBarOrientation RIGHT_TO_LEFT = new ProgressBarOrientation(GtkProgressBarOrientation.RIGHT_TO_LEFT, "RIGHT_TO_LEFT");
+    /*
+     * Perform progress from top to bottom
+     */
     public static final ProgressBarOrientation TOP_TO_BOTTOM = new ProgressBarOrientation(GtkProgressBarOrientation.TOP_TO_BOTTOM, "TOP_TO_BOTTOM");
+    /*
+     * Perform progress from bottom to top
+     */
     public static final ProgressBarOrientation BOTTOM_TO_TOP = new ProgressBarOrientation(GtkProgressBarOrientation.BOTTOM_TO_TOP, "BOTTOM_TO_TOP");
     
     private ProgressBarOrientation(int ordinal, String nickname) {
