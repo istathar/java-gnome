3c2a922a003b6b015252d597852f6bbdd1c4b2bc
Kenneth Prugh <ken69267@gmail.com>
2009-02-24 20:32:42 -0500

    Match upstream example colors

--- tests/screenshots/org/freedesktop/cairo/SnapshotOperatorOver.java
+++ tests/screenshots/org/freedesktop/cairo/SnapshotOperatorOver.java
@@ -23,13 +23,13 @@ public class SnapshotOperatorOver extends SnapshotCairo
     }
 
     protected void draw(Context cr) {
-        cr.setSource(0.0, 1.0, 0.0, 0.8);
+        cr.setSource(0.7, 0, 0, 0.8);
         cr.rectangle(15, 10, 50, 50);
         cr.fill();
 
         cr.setOperator(Operator.OVER);
 
-        cr.setSource(1.0, 0.0, 0.0, 0.8);
+        cr.setSource(0, 0, 0.9, 0.4);
         cr.rectangle(35, 35, 50, 50);
         cr.fill();
     }
