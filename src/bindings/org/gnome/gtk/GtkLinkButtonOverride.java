0f35232590feb10dfa513ef6c4ce553fb0a65776
Serkan Kaba <serkan@gentoo.org>
2009-06-28 06:28:42 +0300

    Fix/add comments in GtkLinkButtonOverride.java

--- src/bindings/org/gnome/gtk/GtkLinkButtonOverride.java
+++ src/bindings/org/gnome/gtk/GtkLinkButtonOverride.java
@@ -1,9 +1,20 @@
+/*
+ * GtkLinkButtonOverride.java
+ *
+ * Copyright (c) 2009 Operational Dynamics Consulting Pty Ltd, and Others
+ * 
+ * The code in this file, and the library it is a part of, are made available
+ * to you by the authors under the terms of the "GNU General Public Licence,
+ * version 2" plus the "Classpath Exception" (you may link to this code as a
+ * library into other programs provided you don't make a derivation of it).
+ * See the LICENCE file for the terms governing usage and redistribution.
+ */
 package org.gnome.gtk;
 
 final class GtkLinkButtonOverride extends Plumbing
 {
     /**
-     * Manually hookup the function that will emit our custom visible signal.
+     * Manually hookup the function that will emit our custom uri-clicked signal.
      */
     static final void setUriHook(LinkButton self) {
         gtk_link_button_set_uri_hook(pointerOf(self));
