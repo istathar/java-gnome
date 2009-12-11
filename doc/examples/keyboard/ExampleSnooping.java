1d7118ed81af9537536e53ebfbf25480fcfdf4ae
Andrew Cowie <andrew@operationaldynamics.com>
2009-12-11 23:47:14 +1100

    Fix typos

--- doc/examples/keyboard/ExampleSnooping.java
+++ doc/examples/keyboard/ExampleSnooping.java
@@ -1,7 +1,7 @@
 /*
  * ExampleSnooping.java
  *
- * Copyright (c) 2008 Operational Dynamics Consulting Pty Ltd
+ * Copyright (c) 2008-2009 Operational Dynamics Consulting Pty Ltd
  *
  * The code in this file, and the program it is a part of, are made available
  * to you by the authors under the terms of the "GNU General Public Licence,
