4a6f23a5e4b3af5d9c34a871ccbb53fb44dcb58f
Andrew Cowie <andrew@operationaldynamics.com>
2010-12-13 13:32:20 +1100

    Update documentation
    Bump since tags to forthcoming release. Correct file to proper GPL
    copyright headers.

--- src/bindings/org/gnome/rsvg/Handle.java
+++ src/bindings/org/gnome/rsvg/Handle.java
@@ -1,17 +1,40 @@
 /*
- * Handle.java
+ * java-gnome, a UI library for writing GTK and GNOME programs from Java!
  *
- * Copyright (c) 2009 Operational Dynamics Consulting Pty Ltd
- * 
- * The code in this file, and the library it is a part of, are made available
- * to you by the authors under the terms of the "GNU General Public Licence,
- * version 2" plus the "Classpath Exception" (you may link to this code as a
- * library into other programs provided you don't make a derivation of it).
- * See the LICENCE file for the terms governing usage and redistribution.
- * 
- * This is essentially the same treatment as is found in org.gnome.gdk.Pixbuf
+ * Copyright © 2009-2010 Operational Dynamics Consulting, Pty Ltd
+ *
+ * The code in this file, and the program it is a part of, is made available
+ * to you by its authors as open source software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License version
+ * 2 ("GPL") as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL for more details.
+ *
+ * You should have received a copy of the GPL along with this program. If not,
+ * see http://www.gnu.org/licenses/. The authors of this program may be
+ * contacted through http://java-gnome.sourceforge.net/.
+ *
+ * Linking this library statically or dynamically with other modules is making
+ * a combined work based on this library. Thus, the terms and conditions of
+ * the GPL cover the whole combination. As a special exception (the
+ * "Claspath Exception"), the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules,
+ * and to copy and distribute the resulting executable under terms of your
+ * choice, provided that you also meet, for each linked independent module,
+ * the terms and conditions of the license of that module. An independent
+ * module is a module which is not derived from or based on this library. If
+ * you modify this library, you may extend the Classpath Exception to your
+ * version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.gnome.rsvg;
+
+/*
+ * This is essentially the same treatment as is found in org.gnome.gdk.Pixbuf
+ */
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,10 +43,23 @@ import java.io.IOException;
 import org.gnome.glib.GlibException;
 
 /**
- * Handle to an SVG image in memory.
+ * Handle to an SVG image in memory. This class allows you to load an SVG
+ * image from a file on disk or from memory.
+ * 
+ * <p>
+ * Once you have the Handle you can render it to Cairo directly with Context's
+ * {@link org.freedesktop.cairo.Context#showHandle(Handle) showHandle()},
+ * 
+ * <pre>
+ * Rsvg.init();
+ * ...
+ * 
+ * handle = new Handle(&quot;Tiger.svg&quot;);
+ * cr.showHandle(handle);
+ * </pre>
  * 
  * @author Andrew Cowie
- * @since 4.0.15
+ * @since 4.0.18
  */
 public class Handle extends org.gnome.glib.Object
 {
@@ -101,7 +137,7 @@ public class Handle extends org.gnome.glib.Object
     /**
      * Get the size of the SVG image represented by this Handle.
      * 
-     * @since 4.0.15
+     * @since 4.0.18
      */
     public DimensionData getDimensions() {
         final DimensionData size;
@@ -120,7 +156,7 @@ public class Handle extends org.gnome.glib.Object
      * <p>
      * You can pass <code>null<code> to get the size of the entire graphic.
      * 
-     * @since 4.0.15
+     * @since 4.0.18
      */
     public DimensionData getDimensionsSub(String id) {
         final DimensionData size;
