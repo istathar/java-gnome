3a44dec28659d836d19aa34650ddc6bacbd3498f
Andrew Cowie <andrew@operationaldynamics.com>
2010-12-18 19:00:46 +1100

    Convert experiments into proper test case
    Having written test output, scan the SVG file for the string
    identifying the expected MIME type.

--- tests/bindings/org/freedesktop/cairo/ValidateCairoInternals.java
+++ tests/bindings/org/freedesktop/cairo/ValidateCairoInternals.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.gnome.gdk.Pixbuf;
 import org.gnome.gtk.GraphicalTestCase;
 
 /**
@@ -29,6 +30,7 @@ import org.gnome.gtk.GraphicalTestCase;
  * turning Cairo sub types into concrete Java objects.
  * 
  * @author Andrew Cowie
+ * @author Adrian Johnson
  */
 public class ValidateCairoInternals extends GraphicalTestCase
 {
@@ -106,23 +108,114 @@ public class ValidateCairoInternals extends GraphicalTestCase
         return data;
     }
 
-    public final void testUsingMimeData() throws IOException {
-        final Surface s1;
-        final Context cr;
+    private static final void assertFileContains(String target, String filename) throws IOException {
         final byte[] data;
-        final Surface s2;
+        final String text;
 
-        s1 = new SvgSurface("tmp/tests/org/freedesktop/cairo/MimeType.svg", 100, 100);
-        cr = new Context(s1);
+        data = readFileIntoArray(filename);
 
-        s2 = s1.createSimilar(Content.COLOR_ALPHA, 100, 100);
+        text = new String(data);
+
+        assertTrue("Output file does not contain \"" + target + "\"", text.contains(target));
+    }
+
+    public final void testUsingMimeDataExplicit() throws IOException {
+        final Surface s1, s2;
+        final Context c1, c2;
+        final Pixbuf pixbuf;
+        final byte[] data;
+
+        /*
+         * Desired output target.
+         */
+
+        s1 = new SvgSurface("tmp/tests/org/freedesktop/cairo/MimeType1.svg", 100, 100);
+        c1 = new Context(s1);
+
+        /*
+         * Get our image into a GDK Pixbuf, but instead of directly loading as
+         * usual load it into Java memory first so we can reuse it in a
+         * minute.
+         */
 
         data = readFileIntoArray("tests/prototype/MapleSyrup.jpg");
+        pixbuf = new Pixbuf(data);
+
+        /*
+         * Intermediate surface. What a bother.
+         */
+
+        s2 = new ImageSurface(Format.ARGB32, 100, 100);
+        c2 = new Context(s2);
+
+        c2.setSource(pixbuf, 0, 0);
+        c2.paint();
+
         s2.setMimeData(MimeType.JPEG, data);
 
-        cr.setSource(s2, 0, 0);
-        cr.paint();
+        /*
+         * Paint the image onto our real surface.
+         */
+
+        c1.setSource(s2, 0, 0);
+        c1.paint();
 
         s1.finish();
+
+        /*
+         * Now test
+         */
+
+        assertFileContains("image/jpeg", "tmp/tests/org/freedesktop/cairo/MimeType1.svg");
+    }
+
+    /**
+     * Thanks for the help of Adrian Johnson. In trying this pattern.
+     */
+    public final void testUsingMimeDataViaPattern() throws IOException {
+        final Surface surface, implicit;
+        final Context cr;
+        final Pattern pattern;
+        final byte[] data;
+        final Pixbuf pixbuf;
+
+        /*
+         * Desired output target.
+         */
+
+        surface = new SvgSurface("tmp/tests/org/freedesktop/cairo/MimeType2.svg", 100, 100);
+        cr = new Context(surface);
+
+        data = readFileIntoArray("tests/prototype/MapleSyrup.jpg");
+        pixbuf = new Pixbuf(data);
+
+        /*
+         * Set the source using the gdk_cairo_set_source_pixbuf() utility, but
+         * do NOT draw it yet.
+         */
+
+        cr.setSource(pixbuf, 0, 0);
+
+        /*
+         * Get the implicitly created Surface, and set the mime data.
+         */
+
+        pattern = cr.getSource();
+        implicit = pattern.getSource();
+        implicit.setMimeData(MimeType.JPEG, data);
+
+        /*
+         * Now you can paint.
+         */
+
+        cr.paint();
+
+        surface.finish();
+
+        /*
+         * Now test
+         */
+
+        assertFileContains("image/jpeg", "tmp/tests/org/freedesktop/cairo/MimeType2.svg");
     }
 }
