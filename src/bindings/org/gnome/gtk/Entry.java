09b9822a0452ba177c3a5af1c397176ff6acb02e
Andrew Cowie <andrew@operationaldynamics.com>
2011-02-13 12:03:19 +1100

    Fix missing inline markup closing tag

--- src/bindings/org/gnome/gtk/Entry.java
+++ src/bindings/org/gnome/gtk/Entry.java
@@ -1,7 +1,7 @@
 /*
  * java-gnome, a UI library for writing GTK and GNOME programs from Java!
  *
- * Copyright © 2007-2010 Operational Dynamics Consulting, Pty Ltd and Others
+ * Copyright © 2007-2011 Operational Dynamics Consulting, Pty Ltd and Others
  *
  * The code in this file, and the program it is a part of, is made available
  * to you by its authors as open source software: you can redistribute it
@@ -648,7 +648,7 @@ public class Entry extends Widget implements Editable, CellEditable
      * 
      * <p>
      * If <code>x</code> or <code>y</code> are not inside an icon,
-     * <code>-1<code> will be returned.
+     * <code>-1</code> will be returned.
      * 
      * @since 4.0.13
      */
