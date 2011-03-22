e74ea8cbac4e2ec7b8f48f0dd16136c6fc975009
Andrew Cowie <andrew@operationaldynamics.com>
2011-03-22 12:02:01 +1100

    Replace field access with strong setter
    Use gtk_window_set_has_user_ref_count() instead of accessing the now
    sealed window->has_user_ref_count directly. Thanks to Matthias Clasen
    for exposing this method in GTK 3.0 on our behalf.

--- src/bindings/org/gnome/gtk/GtkWindowOverride.c
+++ src/bindings/org/gnome/gtk/GtkWindowOverride.c
@@ -1,7 +1,7 @@
 /*
  * java-gnome, a UI library for writing GTK and GNOME programs from Java!
  *
- * Copyright © 2007-2010 Operational Dynamics Consulting, Pty Ltd
+ * Copyright © 2007-2011 Operational Dynamics Consulting, Pty Ltd
  *
  * The code in this file, and the program it is a part of, is made available
  * to you by its authors as open source software: you can redistribute it
@@ -189,7 +189,7 @@ Java_org_gnome_gtk_GtkWindowOverride_gtk_1window_1drop_1user_1ref
 	 * GTK places in GtkWindows.
 	 */
 
-	self->has_user_ref_count = FALSE;
+	gtk_window_set_has_user_ref_count(self, FALSE);
 	g_object_unref(self);
 
 	// cleanup parameter self
